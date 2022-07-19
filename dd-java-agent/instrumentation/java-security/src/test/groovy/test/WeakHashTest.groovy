0f6fd8586d10e38a72e4e6f9efceeabb8ef962dc
jsantos <javier.santos@datadoghq.com>
2022-07-19 17:26:05 +0200

    Cleaner test code

--- dd-java-agent/instrumentation/java-security/src/test/groovy/test/WeakHashTest.groovy
+++ dd-java-agent/instrumentation/java-security/src/test/groovy/test/WeakHashTest.groovy
@@ -6,33 +6,32 @@ import java.security.MessageDigest
 
 import static datadog.trace.agent.test.utils.TraceUtils.runUnderTrace
 
-class WeakHashTest extends AgentTestRunner{
+class WeakHashTest extends AgentTestRunner {
 
   def "test instrumentation"() {
-    setup:
-    List<String> algorithms = Arrays.asList("MD2","MD5","SHA","SHA1","md2","md5","sha","sha1")
 
     when:
-
     runUnderTrace("WeakHashingRootSpan") {
-      for (String algorithm:algorithms) {
-        MessageDigest.getInstance(algorithm)
-      }
+      MessageDigest.getInstance(algorithm)
     }
 
     then:
     assertTraces(1, true) {
-      trace(algorithms.size() + 1){
-        span{resourceName "WeakHashingRootSpan"}
-        span{resourceName "WeakHashingAlgorithm_sha1"}
-        span{resourceName "WeakHashingAlgorithm_sha"}
-        span{resourceName "WeakHashingAlgorithm_md5"}
-        span{resourceName "WeakHashingAlgorithm_md2"}
-        span{resourceName "WeakHashingAlgorithm_SHA1"}
-        span{resourceName "WeakHashingAlgorithm_SHA"}
-        span{resourceName "WeakHashingAlgorithm_MD5"}
-        span{resourceName "WeakHashingAlgorithm_MD2"}
+      trace(2) {
+        span { resourceName "WeakHashingRootSpan" }
+        span { resourceName "WeakHashingAlgorithm_" + algorithm }
       }
     }
+
+    where:
+    algorithm | _
+    "MD2"     | _
+    "MD5"     | _
+    "SHA"     | _
+    "SHA1"    | _
+    "md2"     | _
+    "md5"     | _
+    "sha"     | _
+    "sha1"    | _
   }
 }
